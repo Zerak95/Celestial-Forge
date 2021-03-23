226622d677ad169b70523a0cd5e8b83c98e19f75
Zaid Soboh <zaidsoboh@hotmail.com>
2021-03-23 19:33:43 +1100

    eddited code to acount for extra_perks

--- src/components/Perk/Perk.js
+++ src/components/Perk/Perk.js
@@ -2,23 +2,30 @@ import React from 'react';
 import classes from './Perk.css';
 import Header from './Header/Header';
 import Aux from '../../hoc/Aux'
+import SinglePerk from './SinglePerk';
 
 
 const perk = (props) => {
     // console.log(props.origin);
     let perk = null
 
+    // console.log('THIS is ther perk you selcted');
+    // console.log(props.data);
+    
+    //TODO: tell user when the data is null
     if(props.data !== null){
-        perk = (
-            <Aux>
-                <div className='Perk'>
-                    <Header size={2} >{props.data.name}</Header>
-                    <Header size={3} >{props.data.origin}   ({props.data.cost}cp)</Header>
-                    <p>{props.data.description}</p>
-                </div>
-                {props.children}
-            </Aux>
-        )
+        if (props.data.single_perk) {
+            // console.log('This is a single perk');
+            perk = (
+                <SinglePerk data={props.data}/>
+            )
+        }else{
+            perk = props.data.extra_perks.map(perk => {
+                return (<SinglePerk data={perk}/>);
+            });
+            // console.log('This is NOT NOT a single perk');
+            // console.log(props.data.extra_perks);
+        }
     }
     return (
         perk
