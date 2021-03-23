485a7ef0f6a5c27f479a77a6e409deb5f3c23917
Zaid Soboh <zaidsoboh@hotmail.com>
2021-03-23 19:46:23 +1100

    random function added

--- src/containers/AllPerks/AllPerks.js
+++ src/containers/AllPerks/AllPerks.js
@@ -2,44 +2,68 @@ import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
 import Perk from '../../components/Perk/Perk';
-import perkData from '../../components/data/main-perks.json';
+// import perkData from '../../components/data/main-perks.json';
 import PerkList from '../../components/PerkList/PerkList';
 import Modal from '../../components/UI/Modal/Modal';
 import Backdrop from '../../components/UI/Backdrop/Backdrop';
 
+//TODO: delete this before launch [just for testing]
+import perkData from '../../components/data/test.json';
+
 class AllPerks extends Component {
     state = {
         selectedPerk: null,
-        showPerk: false
+        showPerk: false,
+        currentCP: 800
     }
 
-    showFullPerk = (perk) => {
+    showFullPerkHandler = (perk) => {
         this.setState({selectedPerk: perk, showPerk: true});
-        console.log(perk);
     }
     
-    cancelFullPerk = () => {
+    cancelFullPerkHandler = () => {
         this.setState({selectedPerk: null, showPerk: false});
     }
 
+    randomPerkHandler = (listOfPerks) => {
+        
+        //TODO: if not single perk and there are enough points random till one that can be aforded is picked.
+        
+        let randomPerk = listOfPerks[Math.floor(Math.random() * listOfPerks.length)];
+        
+        if (typeof(randomPerk) == "undefined") {
+            return;
+        }
+        
+        if(typeof(randomPerk.single_perk) != "undefined" && !randomPerk.single_perk){
+            this.randomPerkHandler(randomPerk.extra_perks);
+        }else{
+            this.setState({selectedPerk: randomPerk, showPerk: true});
+        }
+        
+        //TODO: account for when there CP for perk is not enough
+        
+    }
+
+    //TODO: fix display
     
     
     render () {
-        console.log(perkData);
-        console.log(Object.values(perkData));
         return(
             <Aux>
                 <Backdrop></Backdrop>
 
-                <Modal show={this.state.showPerk} modalClosed={this.cancelFullPerk}>   
+                <Modal show={this.state.showPerk} modalClosed={this.cancelFullPerkHandler}>   
                     <Perk
                         data={this.state.selectedPerk}
                     /> 
                 </Modal>
 
+                <button onClick={this.randomPerkHandler.bind(this, Object.values(perkData))}>Random</button>
+
                 <PerkList 
                     data={Object.values(perkData)} 
-                    clicked={this.showFullPerk}
+                    clicked={this.showFullPerkHandler}
                 />
 
             </Aux>
