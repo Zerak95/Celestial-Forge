e63005d3d41ae6c1d365ad33e2ae66e6f4be1c63
Zaid Soboh <zaidsoboh@hotmail.com>
2021-03-23 19:42:35 +1100

    bug fixed [not having "single_perk' in the extra perks crashes the program]

--- src/components/Perk/Perk.js
+++ src/components/Perk/Perk.js
@@ -6,25 +6,19 @@ import SinglePerk from './SinglePerk';
 
 
 const perk = (props) => {
-    // console.log(props.origin);
     let perk = null
 
-    // console.log('THIS is ther perk you selcted');
-    // console.log(props.data);
     
-    //TODO: tell user when the data is null
+    //TODO: tell user when the data is or undifined 
     if(props.data !== null){
-        if (props.data.single_perk) {
-            // console.log('This is a single perk');
-            perk = (
-                <SinglePerk data={props.data}/>
-            )
-        }else{
+        if (!props.data.single_perk) {
             perk = props.data.extra_perks.map(perk => {
                 return (<SinglePerk data={perk}/>);
             });
-            // console.log('This is NOT NOT a single perk');
-            // console.log(props.data.extra_perks);
+        }else{
+            perk = (
+                <SinglePerk data={props.data}/>
+            )
         }
     }
     return (
