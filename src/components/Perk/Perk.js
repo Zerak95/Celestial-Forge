55dda1fd3ce8b15fa88477fa1e52d53cd53771a1
Zaid Soboh <zaidsoboh@hotmail.com>
2021-04-09 18:09:09 +1000

    perks now have a button to add and remove perks

--- src/components/Perk/Perk.js
+++ src/components/Perk/Perk.js
@@ -10,14 +10,27 @@ const perk = (props) => {
 
     
     //TODO: tell user when the data is or undifined 
+    //TODO: check if there is a way to not have duplicate code
     if(props.data !== null){
         if (!props.data.single_perk) {
             perk = props.data.extra_perks.map(perk => {
-                return (<SinglePerk data={perk}/>);
+                return (
+                    <SinglePerk 
+                        data={perk}
+                        checkIfPerkOwned={props.checkIfPerkOwned}
+                        addPerk={props.addPerk}
+                        removePerk={props.removePerk}
+                    />
+                );
             });
         }else{
             perk = (
-                <SinglePerk data={props.data}/>
+                <SinglePerk 
+                    data={props.data}
+                    checkIfPerkOwned={props.checkIfPerkOwned}
+                    addPerk={props.addPerk}
+                    removePerk={props.removePerk}
+                />
             )
         }
     }
