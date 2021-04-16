91ca6f76acf0366dba76490c3cf193c9972d0c1f
Zaid Soboh <zaidsoboh@hotmail.com>
2021-04-17 09:14:03 +1000

    Layout changed back to functional component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,30 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Aux from '../../hoc/Aux';
 import NavBar from '../UI/Navigation/NavBar/NavBar';
 import classes from './Layout.css';
 
 //TODO: change to class when state is added. Also change props to this.props
-class Layout extends Component {
-    state = {
-        perkListToDisplay: 1
-    }
-
-    listOfPerksToDisplayHandler = listNumber => {
-        if (listNumber !== this.state.perkListToDisplay) {
-            this.setState({perkListToDisplay:listNumber});
-        }
-    }
-
-    render () {
-        return (
-            <Aux>
-                <NavBar/>
-                <main className='Content'>
-                    {props.children}
-                </main>
-            </Aux>
-        );
-    }
+const layout = (props) => {
+    return (
+        <Aux>
+            <NavBar/>
+            <main className='Content'>
+                {props.children}
+            </main>
+        </Aux>
+    )
 }
 
-export default Layout;
+export default layout;
