3335b53e1bbd2afaeb7d3730e4884c1b68029bd0
Zaid Soboh <zaidsoboh@hotmail.com>
2021-04-17 09:04:28 +1000

    changed Layout to a component and added handler for list of perks to display

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
+import NavBar from '../UI/Navigation/NavBar/NavBar';
+import classes from './Layout.css';
 
 //TODO: change to class when state is added. Also change props to this.props
-const layout = (props) => {
-    return (
-        <Aux>
-            <main>
-                {props.children}
-            </main>
-        </Aux>
-    )
+class Layout extends Component {
+    state = {
+        perkListToDisplay: 1
+    }
+
+    listOfPerksToDisplayHandler = listNumber => {
+        if (listNumber !== this.state.perkListToDisplay) {
+            this.setState({perkListToDisplay:listNumber});
+        }
+    }
+
+    render () {
+        return (
+            <Aux>
+                <NavBar/>
+                <main className='Content'>
+                    {props.children}
+                </main>
+            </Aux>
+        );
+    }
 }
 
-export default layout;
+export default Layout;
